fb94ef8730c0c71cf5d3858a6c9cbdbc03515be7
pjgowtham <pjgowtham@gmail.com>
2023-03-13 11:17:05 +0530

    cupid: Initial rebrand for realme GT
    Change-Id: I16d98db183a6adf00fc09f053822f68b7ea997b4

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lemonade.mk
+    $(LOCAL_DIR)/lineage_cupidr.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lemonade-user \
-    lineage_lemonade-userdebug \
-    lineage_lemonade-eng
+    lineage_cupidr-user \
+    lineage_cupidr-userdebug \
+    lineage_cupidr-eng
