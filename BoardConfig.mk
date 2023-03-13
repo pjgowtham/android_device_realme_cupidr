b99dceb0d647153732f1f20cfa5e38f80b770183
pjgowtham <pjgowtham@gmail.com>
2023-03-13 11:17:05 +0530

    cupid: Initial rebrand for realme GT
    Change-Id: I16d98db183a6adf00fc09f053822f68b7ea997b4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,7 +7,7 @@
 # Include the common OEM chipset BoardConfig.
 include device/oneplus/sm8350-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/oneplus/lemonade
+DEVICE_PATH := device/realme/cupidr
 
 # Display
 TARGET_SCREEN_DENSITY := 450
@@ -18,6 +18,10 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 # Lineage Health
 TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/oplus_chg/battery/chg_enable
 
+# Partitions
+BOARD_ONEPLUS_DYNAMIC_PARTITIONS_SIZE := 10196353024 # BOARD_SUPER_PARTITION_SIZE - 4MB
+BOARD_SUPER_PARTITION_SIZE := 10200547328
+
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
@@ -25,4 +29,4 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
 
 # Include the proprietary files BoardConfig.
-include vendor/oneplus/lemonade/BoardConfigVendor.mk
+include vendor/realme/cupidr/BoardConfigVendor.mk
