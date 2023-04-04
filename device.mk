4e3ca1d3c1e279ef74ee72afa4ef33baf1999c02
pjgowtham <pjgowtham@gmail.com>
2023-04-04 11:11:43 +0530

    cupid: Import sensors hal with als correction from oneplus sm8150-common
     As of commit 981308f9aa0cb93fc307464299421e9f6608e60d
    
    Change-Id: I1faf4bfa8633626d6c6f3b5630be60fe66fbd92d

--- device.mk
+++ device.mk
@@ -50,6 +50,11 @@ PRODUCT_PACKAGES += \
     OPlusSettingsResTarget \
     OPlusSystemUIResTarget
 
+# Sensors
+PRODUCT_PACKAGES += \
+    als_correction_service.cupidr \
+    android.hardware.sensors@2.0-service.multihal.cupidr
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
