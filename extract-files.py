7faadf2024431c28b806a3f5171b2f28cbe9df66
pjgowtham <pjgowtham@gmail.com>
2025-01-18 21:31:32 +0530

    cupidr: Update from RMX2202_14.0.0.1700(EX01)
    Change-Id: Ib9f2fd1c5ef87977b67dc78a56c0a38be7f521b5

--- extract-files.py
+++ extract-files.py
@@ -73,7 +73,7 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('SystemCamera =  0;  0;  1;  1;  1', 'SystemCamera =  0;  0;  0;  0;  1'),
     ('odm/lib/liblvimfs_wrapper.so', 'odm/lib64/libCOppLceTonemapAPI.so', 'odm/lib64/libaps_frame_registration.so', 'vendor/lib64/libalsc.so'): blob_fixup()
         .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
-    ('odm/lib/libdehaze.so', 'odm/lib64/libarcsoft_hdrplus_hvx_stub.so', 'odm/lib64/libarcsoft_high_dynamic_range_v4.so', 'odm/lib64/libarcsoft_portrait_super_night_raw.so', 'odm/lib64/libarcsoft_super_night_raw.so'): blob_fixup()
+    ('odm/lib/libdehaze.so', 'odm/lib64/libarcsoft_high_dynamic_range_v4.so', 'odm/lib64/libarcsoft_portrait_super_night_raw.so', 'odm/lib64/libarcsoft_super_night_raw.so'): blob_fixup()
         .clear_symbol_version('remote_handle_close')
         .clear_symbol_version('remote_handle_invoke')
         .clear_symbol_version('remote_handle_open')
